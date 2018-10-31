f51d2535907344774fbd9cc8eb1bb61cb379fa9b
Roald <roaldb86@gmail.com>
2018-10-31 12:02:38 +0100

    changed path

--- worldbank.py
+++ worldbank.py
@@ -42,7 +42,7 @@ except:
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('./worldbankapp/templates/index.html')
+    return render_template('index.html')
 
 @app.route('/disaster')
 def disaster():
