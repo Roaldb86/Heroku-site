1e09caea068ad3d284388cb34f6ffaab4cb9522c
Roald <roaldb86@gmail.com>
2018-11-05 14:05:48 +0100

    only load tokenize from trainingscript

--- worldbank.py
+++ worldbank.py
@@ -4,7 +4,7 @@ import pandas as pd
 import nltk
 import pickle
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -18,7 +18,7 @@ from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
-from models.train_classifier_for_web import tokenize, main
+from models.train_classifier_for_web import tokenize
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -27,16 +27,16 @@ nltk.download('wordnet')
 app = Flask(__name__)
 
 
-def tokenize(text):
-    """Takes a text as input an returns a list of tokenized words"""
-    stop_words = stopwords.words("english")
-    text = re.sub(r"[^a-zA-Z0-9]", " ", text).lower().strip()
-    words = word_tokenize(text)
-    clean_words = [w for w in words if w not in stopwords.words("english")]
-    tokens = [WordNetLemmatizer().lemmatize(w) for w in words if w not in stop_words]
-    return [PorterStemmer().stem(w) for w in tokens]
-
-    return clean_tokens
+# def tokenize(text):
+#     """Takes a text as input an returns a list of tokenized words"""
+#     stop_words = stopwords.words("english")
+#     text = re.sub(r"[^a-zA-Z0-9]", " ", text).lower().strip()
+#     words = word_tokenize(text)
+#     clean_words = [w for w in words if w not in stopwords.words("english")]
+#     tokens = [WordNetLemmatizer().lemmatize(w) for w in words if w not in stop_words]
+#     return [PorterStemmer().stem(w) for w in tokens]
+#
+#     return clean_tokens
 
 # load data
 try:
