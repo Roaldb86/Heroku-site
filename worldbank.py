220c506ca5838c65739151e531a7199596b0cd7a
Roald <roaldb86@gmail.com>
2018-11-05 14:54:08 +0100

    new trial with global vars

--- worldbank.py
+++ worldbank.py
@@ -39,6 +39,9 @@ app = Flask(__name__)
 #     return clean_tokens
 
 def main():
+    global df
+    global model
+
     try:
         engine = create_engine('sqlite:///DisasterResponse.db')
         df = pd.read_sql_table('disaster_data', engine)
@@ -49,7 +52,7 @@ def main():
     except Exception as e:
         print("cant load model", e)
 
-    return df, model
+
 
 
 @app.route('/')
@@ -63,7 +66,7 @@ def flowers():
 
 @app.route('/disaster')
 def disaster():
-
+    global df
         ## extract data needed for visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
@@ -140,7 +143,7 @@ def disaster():
 
 @app.route('/go')
 def go():
-
+    global model
     # save user input in query
     query = request.args.get('query', '')
 
@@ -155,4 +158,4 @@ def go():
         classification_result=classification_results)
 
 if __name__ == '__main__':
-    df, model = main()
+    main()
